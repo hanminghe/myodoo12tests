af6e630504723ec70d119841087c01afd72871e6
hanminghe <hanminghe@shugia.com>
2019-09-08 19:17:25 +0800

    model inheritant

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -87,7 +87,6 @@ class LibraryBook(models.Model):
     @api.model
     def _referencable_models(self):
         models = self.env['ir.model'].search([('field_id.name', '=', 'message_ids')])
-        print(models)
         return [(x.model, x.name) for x in models]
 
     def name_get(self):
@@ -116,3 +115,9 @@ class ResPartner(models.Model):
         string='Authored Books',
         # relation='library_book_res_partner_rel'  # optional
     )
+    count_books = fields.Integer('Number of Authored Books', compute='_compute_count_books')
+
+    @api.depends('authored_book_ids')
+    def _compute_count_books(self):
+        for r in self:
+            r.count_books = len(r.authored_book_ids)
